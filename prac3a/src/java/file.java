59068d62fab9d53ee5ed34395139aaf89296f48b
HastiSanghani <hasti.sanghani04@gmail.com>
2024-07-12 12:31:43 +0530

    Add files via upload

---
+++ prac3a/src/java/file.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Admin
+ */
+class file {
+
+    static String separater;
+    static String separator;
+
+    file(String string) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
